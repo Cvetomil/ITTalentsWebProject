72b92ccdb943dcaa171887a427fd118780ad42ce
Cvetomil <cvetikostov@gmail.com>
2019-02-26 12:35:21 +0200

    update

--- src/main/java/ittalents/webappsports/controllers/UserController.java
+++ src/main/java/ittalents/webappsports/controllers/UserController.java
@@ -1,9 +1,13 @@
 package ittalents.webappsports.controllers;
 
 
+import ittalents.webappsports.models.User;
 import ittalents.webappsports.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,4 +18,11 @@ public class UserController {
 
     @Autowired
     UserRepository userRepository;
+
+    @PostMapping("/register")
+    public String registerUser(@RequestBody User user){
+        userRepository.save(user);
+        return "You have been registered successfully";
+    }
+
 }
